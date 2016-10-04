14ebe8dc4b4d031845aa1d60d7d26766cc8ead67
mgoofyy <mgoofyy@gmail.com>
2016-10-05 00:20:54 +0800

    测试数据库列表

--- db/dbManger.js
+++ db/dbManger.js
@@ -2,9 +2,10 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
         host : '127.0.0.1',
-        use : 'root',
-        pass : 'mysql',
+        user : 'root',
+        password : 'mysql',
         port : '3306',
+        database: 'oneWord',
     });
 
 var DBManger = {
@@ -12,16 +13,33 @@ var DBManger = {
     //连接到数据库
     open:function(callback) {
         connection.connect(function(error){
-            !error ? nil : callback(error);
-            return;
+            if(error) {
+                callback(error);
+                return;
+            }
+        console.log('连接数据库成功');
+        });
+    },
+
+    //执行sql语句
+    query : function(queryString,callback) {
+        connection.query(queryString,function(error,rows,fields){
+            if(error){
+                callback(error);
+                return;
+            }
+            console.log(queryString + "操作数据库成功");
         });
     },
 
     //关闭数据库
     close:function(callback) {
         connection.end(function(error){
-            !error ? nil : callback(error);
-            return;
+            if(error) {
+                callback(error);
+                return;
+            }
+        console.log('关闭连接数据库成功');
         });
     }
 };
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var db = require('./../db/dbManger');
+var User = require('./../db/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,17 +9,30 @@ router.get('/', function(req, res, next) {
 
 // POST 请求登陆
 router.post('/login',function(req,res,next){
-  
+  // var requestParams = req.body;
+  var user = User.createUser();
+  console.log(typeof user);
+  user.nickname='coffee';
+  user.age=13;
+  user.place = 'american';
+  user.sex='2';
+  user.signature='go ur rord, let others to say ';
+  user.phone = '1881418410';
+  user.userType=1;
+  User.save(user,function(err){
+    console.log(err);
+  });
+  res.send('respond with a resource');
 });
 
 // POST 请求注册
 router.post('/signup',function(req,res,next){
-  
+  res.send('respond with a resource');
 });
 
 // POST 获取用户信息
 router.post('/info',function(req,res,nex){
-
+res.send('respond with a resource');
 });
 
 module.exports = router;
